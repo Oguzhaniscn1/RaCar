4c2a2ec33415b03dfe98d9d95f450fc22479cd49
Oğuzhan Işcan <oguzhaniscn28@gmail.com>
2023-10-25 19:42:44 +0300

    Controllers Added

---
+++ WebAPI/Controllers/CustomersController.cs
@@ -0,0 +1,76 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        ICustomerService _customerService;
+        public CustomersController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _customerService.GetAllCustomers();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest();
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Customer customer)
+        {
+            var result = _customerService.AddCustomer(customer);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest();
+        }
+
+        [HttpPost("update")]
+        public IActionResult Update(Customer customer)
+        {
+            var result = _customerService.UpdateCustomer(customer);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest();
+        }
+
+        [HttpPost("delete")]
+        public IActionResult Delete(Customer customer)
+        {
+            var result = _customerService.DeleteCustomer(customer);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest();
+        }
+
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _customerService.GetCustomerById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest();
+        }
+
+    }
+}
---
+++ WebAPI/Controllers/RentalsController.cs
@@ -0,0 +1,77 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RentalsController : ControllerBase
+    {
+        IRentalService _rentalService;
+        public RentalsController(IRentalService rentalService)
+        {
+            _rentalService = rentalService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll() 
+        {
+            var result = _rentalService.GetAllRentals();
+            if(result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest();
+        }
+
+        [HttpPost("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var resul=_rentalService.GetRantalById(id);
+            if(resul.Success)
+            {
+                return Ok(resul);
+            }
+            return BadRequest();
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Rental rental)
+        {
+            var result=_rentalService.AddRental(rental);
+            if(result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest();
+        
+        }
+
+        [HttpPost("delete")]
+        public IActionResult Delete(Rental rental)
+        {
+            var result=_rentalService.DeleteRental(rental);
+            if(result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest();
+        }
+
+        [HttpPost("update")]
+        public IActionResult Update(Rental renral)
+        {
+            var result = _rentalService.UpdateRental(renral);
+            if(result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest();
+        }
+
+
+
+    }
+}
